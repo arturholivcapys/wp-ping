85c6f191b060bd428a298d823880c1722faf63ab
arturholivcapys <artur.henrique45@hotmail.com>
2024-09-23 16:58:27 -0300

    feat: changes base route

--- wp-ping/src/app.js
+++ wp-ping/src/app.js
@@ -1,7 +1,7 @@
 const { app, PORT } = require("./config");
 const messageRoutes = require("./routes/messageRoutes");
 
-app.use("/api/messages", messageRoutes);
+app.use("/api", messageRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
